8a6fb2b7d12ff932c331b896a51832a3b0196c71
vjdv <vjdv@outlook.com>
2020-07-07 20:37:18 +0100

    obtiene ip de interfaces

--- display.py
+++ display.py
@@ -3,6 +3,7 @@
 import threading
 from datetime import datetime
 import psutil
+import netifaces as ni
 
 def setInterval(func, time):
   def runner():
@@ -12,6 +13,24 @@ def setInterval(func, time):
   t = threading.Thread(target=runner)
   t.start()
   return t
+
+#System network
+eth0_ip = "9.9.9.9"
+wlan0_ip = "9.9.9.9"
+def updateIps():
+  global eth0_ip
+  global wlan0_ip
+  #eth0_ip = ni.ifaddresses("eth0")[ni.AF_INET][0]["addr"]
+  eth0 = ni.ifaddresses("eth0")
+  if ni.AF_INET in eth0:
+    eth0_ip = eth0_ip[ni.AF_INET][0]["addr"]
+  else:
+    eth0_ip = "--"
+  wlan0 = ni.ifaddresses("wlan0")
+  if ni.AF_INET in wlan0:
+    wlan0_ip = ni.ifaddresses("wlan0")[ni.AF_INET][0]["addr"]
+  else:
+    wlan0_ip = "--"
 
 #System stats
 used_cpu = "0.0%"
@@ -30,6 +49,11 @@ def showSystemStats():
   print "CPU: " + used_cpu
   print "RAM: " + used_memory
   print "Hora: " + current_time
+  print "eth0: " + eth0_ip
+  print "wlan0: " + wlan0_ip
+
+#exec
+updateIps()
 
 #Intervals
 setInterval(updateSystemStats, 5)
