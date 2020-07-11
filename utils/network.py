ba6a62d836f1781c641ebe6f5b38607002055551
vjdv <vjdv@outlook.com>
2020-07-11 05:27:03 +0100

    wifi icon

--- utils/network.py
+++ utils/network.py
@@ -13,10 +13,16 @@ def interfaceStats(interface):
 def showStats(interface, draw, width, height):
     margin = 2
     data = interfaceStats(interface)
-    title_text(draw, margin, width, text=interface + " " + data[0])
+    title_text(draw, margin, width, text=data[0])
     print(data)
-    draw.text((margin, 25), text="Rx: " + data[1], font=tiny_font, fill="white")
-    draw.text((margin, 40), text="Tx: " + data[2], font=tiny_font, fill="white")
+    draw.text((margin, 20), text=interface, fill="white")
+    draw.text((margin, 35), text="Rx: " + data[1], font=tiny_font, fill="white")
+    draw.text((margin, 47), text="Tx: " + data[2], font=tiny_font, fill="white")
+    #dibuja icono wifi
+    if interface.startswith("wlan"):
+      draw.pieslice((80,25,125,70), 225, 315, outline="white", fill="white", width=3)
+      draw.arc((95,40,110,55), 225, 315, fill="black", width=4)
+      draw.arc((85,30,120,65), 225, 315, fill="black", width=5)
 
 def showWlan(draw, width, height):
     showStats("wlan0", draw, width, height)
